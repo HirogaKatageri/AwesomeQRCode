3e2a5fdc8d1083438dad6a5a0ad23e5a59d1fc64
Gian Quintana <gianpatrick27@gmail.com>
2021-12-26 21:19:12 +0800

    1.2.1

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -44,3 +44,42 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.32")
     implementation("com.google.zxing:core:3.2.1")
 }
+
+afterEvaluate {
+    publishing {
+        publications {
+
+            // Creates a Maven publication called "release".
+            create<MavenPublication>("release") {
+
+                // Applies the component for the release build variant.
+                from(components["release"])
+
+                // You can then customize attributes of the publication as shown below.
+                groupId = "dev.hirogakatageri"
+                artifactId = "android-qr-code"
+                version = "1.2.1"
+
+                pom {
+                    name.set("android-qr-code")
+                    description.set("Awesome QR Code by SumiMakito")
+
+                    licenses {
+                        license {
+                            name.set("Apache-2.0")
+                            url.set("https://github.com/HirogaKatageri/AwesomeQRCode/blob/master/LICENSE")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("SumiMakito")
+                            name.set("SumiMakito")
+                            email.set("i@keep.moe")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
